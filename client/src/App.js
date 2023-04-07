2a4ece5505c2ecc21d0f5cdc2a876c4e81bbf9c4
Choccygogo <huyaoyuanqaq@gmail.com>
2023-04-08 04:12:45 +0800

    update admin ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { AppRouter } from './Route'
 import { useRoutes } from 'react-router-dom'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from './theme';
+import 'simplebar-react/dist/simplebar.min.css';
+
 const App = () => {
     const element = useRoutes(AppRouter)
-
-    return <div>{element}</div>
+    const theme = createTheme({
+        colorPreset: 'blue',
+        contrast: 'high'
+      });
+    
+      
+    return (
+        <ThemeProvider theme={theme}>
+         <CssBaseline />
+         {element}
+        </ThemeProvider>)
 }
 
 export default App
---
+++ client/src/admin-pages/index.js
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async';
+import {
+  Box,
+} from '@mui/material';
+
+
+const Page = () => (
+  <>
+    <Helmet> [页面名]
+      <title>
+        Post
+      </title>
+    </Helmet>
+    <Box
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+    </Box>
+  </>
+);
+
+export default Page;
