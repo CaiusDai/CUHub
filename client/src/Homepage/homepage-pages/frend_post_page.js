d773e1d067260ba59f99ff9fae369bc810993033
Jeffrey819 <63940899+Jeffrey819@users.noreply.github.com>
2023-04-10 20:07:18 +0800

    Remove change on front-end

--- client/src/Homepage/homepage-pages/frend_post_page.js
+++ client/src/Homepage/homepage-pages/frend_post_page.js
@@ -2,72 +2,6 @@ import { useCallback, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { subDays } from 'date-fns'
 import { Box, Card, Container, Divider, Stack, Typography } from '@mui/material'
-<<<<<<<< HEAD:client/src/Admin/admin-pages/blocking_list.js
-import { OrdersSearch } from 'src/Admin/admin-sections/orders/orders-search'
-import { OrdersTable } from 'src/Admin/admin-sections/orders/orders-table'
-
-const now = new Date()
-
-//I need backend to provide the mail address(id) and the start & end date of the blocking user.
-//You can refer to the examples already entered below,
-//but of course I will replace them with real user data after you have provided me with the backend user data.
-const orders = [
-    fetch(`http://localhost:5000/api/admin/block/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-    })
-        .then((response) => response.json())
-        .then((res) => {
-            // Format of resposne:
-            const stauts = res.status // status. 'success' for a success operation
-            const data = res.data
-            const message = res.message // Debug only
-            const block_list = data.block_list // Array of {email,startAt,endAt}
-            // Example : for email1
-            const email1 = block_list[0].email
-            const startAt = block_list[0].startAt
-            const endAt = block_list[0].endAt
-            console.log(block_list)
-        }),
-    // {
-    //     id: 'usermail@gmail.com',
-    //     createdAt: subDays(now, 21).getTime(),
-    //     updatedAt: subDays(now, 7).getTime(),
-    // },
-    // {
-    //     id: '9265@xxx',
-    //     createdAt: subDays(now, 56).getTime(),
-    //     status: 'complete',
-    //     updatedAt: subDays(now, 54).getTime(),
-    // },
-    // {
-    //     id: '9266',
-    //     createdAt: subDays(now, 31).getTime(),
-    //     status: 'placed',
-    //     updatedAt: subDays(now, 43).getTime(),
-    // },
-    // {
-    //     id: '1090',
-    //     createdAt: subDays(now, 51).getTime(),
-    //     status: 'processed',
-    //     updatedAt: subDays(now, 13).getTime(),
-    // },
-    // {
-    //     id: '1111',
-    //     createdAt: subDays(now, 6).getTime(),
-    //     status: 'processed',
-    //     updatedAt: subDays(now, 54).getTime(),
-    // },
-    // {
-    //     id: '2475',
-    //     createdAt: subDays(now, 17).getTime(),
-    //     status: 'complete',
-    //     updatedAt: subDays(now, 1).getTime(),
-    // },
-========
 import { PostTable } from 'src/Homepage/homepage-sections/post-table'
 
 const now = new Date()
@@ -108,7 +42,6 @@ const following_posts = [
         updatedAt: subDays(now, 54).getTime(),
         isLiked: true,
     },
->>>>>>>> 908af2342ae3264ab0f5582f2f837ba14648912e:client/src/Homepage/homepage-pages/frend_post_page.js
 ]
 
 const FriendsPost = () => {
